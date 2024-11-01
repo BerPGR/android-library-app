b18861bc237af8fcda2536421788f141c025ff5e
Bernardo de Melo Matuchewski <bernardopgr01@gmail.com>
2024-10-31 22:42:01 -0300

    Feat: Criado repositório

---
+++ app/src/main/java/br/com/libraryapp/repository/Repository.kt
@@ -0,0 +1,11 @@
+package br.com.libraryapp.repository
+
+import br.com.libraryapp.room.BookEntity
+import br.com.libraryapp.room.BooksDB
+
+class Repository(val bookDB: BooksDB) {
+
+    suspend fun addBookToRoom(bookEntity: BookEntity) {
+        bookDB.bookDao().addBook(bookEntity)
+    }
+}
